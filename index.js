c7600341b86724b886d1ef0cf166bb243f0dc63e
GABRIEL CHEDIEX VINER <46916327@ort>
2023-05-02 09:32:28 -0300

    Haciendo que funcione en postman

--- index.js
+++ index.js
@@ -1,21 +1,63 @@
-import config from './dbconfig.js'
-import sql from 'mssql'
-import PizzaService from './src/services/pizzas-services.js'
+import config from './dbconfig.js';
+import sql from 'mssql';
+import PizzaService from './src/services/pizzas-services.js';
 import Pizza from './src/models/Pizza.js';
+import express from 'express';
+
+const app  = express();
+const port = 3000;
 
 
-let pool = await sql.connect(config)
+/*let pool = await sql.connect(config)
 let result = await pool.request().query('')
-let svc = new PizzaService();
-await GetAll(5,'id')
-//let NuevaPizza = new Pizza()
-//NuevaPizza = await GetByID(1)
-//console.log(NuevaPizza);
-//NuevaPizza.Importe = NuevaPizza.Importe * 2;
-//await Insert(NuevaPizza)
+await GetAll(null,'nombre','desc')
+let NuevaPizza = new Pizza()
+NuevaPizza = await GetByID(1)
+console.log(NuevaPizza);
+NuevaPizza.Importe = NuevaPizza.Importe * 2;
+await Insert(NuevaPizza)*/
+
+app.get('/',(req,res) => {
+    res.send('Hola mundo!')
+})
+
+// localhost:3000/pizzas?order=importe
+app.get('/pizzas/getAll/:top/:orderField/:sortOrder', async (req,res) =>{
+    let svc = new PizzaService();
+
+    /*let top = req.query.top
+    let orderField = req.query.orderField
+    let sortOrder = req.query.sortOrder*/
+
+    let resul = await svc.GetAll(req.params.top?? null, req.params.orderField?? null, req.params.sortOrder??null)
+    //console.log(resul)
+    res.send(resul)
+})
+
+/*app.get('/not-found',(req,res) => {
+    res.status(404).send('No se encontró')
+})
+
+app.get('/get-params/:id', (req,res) =>{
+    let parametros = req.query
+    console.log(parametros)
+    res.send('id: ' + req.params.id)
+})*/
+
+app.post({
+
+})
+
+app.put({
+
+})
+
+app.listen(port, () =>{
+    console.log(`App listening on port ${port}`)
+})
 
 
-async function GetAll(top,orderField,sortOrder)
+/*async function GetAll(top,orderField,sortOrder)
 {
     let resul = await svc.GetAll(top,orderField,sortOrder)
     console.log(resul)
@@ -26,7 +68,7 @@ async function GetAll(top,orderField,sortOrder)
 async function GetByID(id)
 {
     let resul = await svc.GetByID(id);
-    //console.log(resul);
+    console.log(resul);
     return resul;
     
 }
@@ -45,4 +87,4 @@ async function Delete(id)
     console.log(resul)
     return resul
     
-}
+}*/
